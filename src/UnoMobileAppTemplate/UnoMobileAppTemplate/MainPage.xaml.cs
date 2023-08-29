76c6eab05803a59b06e55e6edfd9640674136de2
balbarak <balbarak@live.com>
2023-08-29 16:29:48 +0300

    int project files !

---
+++ src/UnoMobileAppTemplate/UnoMobileAppTemplate/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+namespace UnoMobileAppTemplate
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
